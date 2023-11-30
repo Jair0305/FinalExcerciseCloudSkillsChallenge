6ad99e608f20d03ef6c0474852dc17f7948cb4fe
Jair Chavez Islas <73390890+Jair0305@users.noreply.github.com>
2023-11-30 10:25:49 +0000

    first copilot request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+  const [prompt, setPrompt] = useState('');
+
+  const handleImageUrlChange = (event) => {
+    setImageUrl(event.target.value);
+  };
+
+  const handlePromptChange = (event) => {
+    setPrompt(event.target.value);
+  };
+
+  const handleImageAnalysis = () => {
+    // Code to trigger image analysis
+  };
+
+  const handleImageGeneration = () => {
+    // Code to trigger image generation
+  };
+
+  return (
+    <div>
+      <h1>Title</h1>
+      <input type="text" value={imageUrl} onChange={handleImageUrlChange} placeholder="Enter image URL" />
+      <input type="text" value={prompt} onChange={handlePromptChange} placeholder="Enter prompt" />
+      <button onClick={handleImageAnalysis}>Analyze Image</button>
+      <button onClick={handleImageGeneration}>Generate Image</button>
+    </div>
+  );
 }
 
 export default App;
